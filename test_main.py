ee443aa22c672562d317a86fd64840dc56f7b9c3
egorandananev <egorandananev2>
2026-01-22 03:00:02 +0300

    Fixxx

--- test_main.py
+++ test_main.py
@@ -3,16 +3,18 @@ from main import app
 
 client = TestClient(app)
 
+
 def test_health():
     response = client.get("/")
     assert response.status_code == 200
+
 
 def test_metrics():
     response = client.get("/metrics")
     assert response.status_code == 200
     assert "python_gc_objects" in response.text
-    
+
+
 def test_alerts():
     response = client.get("/")
     assert response.status_code == 200
-
